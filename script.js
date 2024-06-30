b26abba423f11f1db06e3a145cf3f896617fa303
Suraj Pawar <173067763+Suraj12072005@users.noreply.github.com>
2024-06-30 15:32:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Clears the screen on click of C button.
+function clearScreen() {
+    document.getElementById("result").value = "";
+  }
+  // Displays entered value on screen.
+  function liveScreen(value) {
+    document.getElementById("result").value += value;
+  }
+ 
